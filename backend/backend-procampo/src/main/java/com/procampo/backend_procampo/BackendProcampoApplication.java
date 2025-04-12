0c9c2ba08133d19386e52ebc503a0e1a72160dad
InakiMerino0 <inakimerino2308@gmail.com>
2025-04-12 13:34:02 -0300

    Inicio del proyecto - estructura base frontend y backend

---
+++ backend/backend-procampo/src/main/java/com/procampo/backend_procampo/BackendProcampoApplication.java
@@ -0,0 +1,13 @@
+package com.procampo.backend_procampo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProcampoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProcampoApplication.class, args);
+	}
+
+}
---
+++ backend/backend-procampo/src/test/java/com/procampo/backend_procampo/BackendProcampoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.procampo.backend_procampo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProcampoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
